45652411d62de532e6f2480f6e02f50bb9e2ff10
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2022-01-27 13:35:41 -0300

    UPDATE - Product-OrderItem one to many association.

--- study-spring-boot/src/main/java/com/github/rod1andrade/studyspringboot/entities/Product.java
+++ study-spring-boot/src/main/java/com/github/rod1andrade/studyspringboot/entities/Product.java
@@ -1,5 +1,6 @@
 package com.github.rod1andrade.studyspringboot.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -7,6 +8,7 @@ import java.io.Serial;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Entidade: Produto
@@ -49,4 +51,12 @@ public class Product implements Serializable {
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     @Getter
     private final Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private final Set<OrderItem> items = new HashSet<>();
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        return items.stream().map(OrderItem::getOrder).collect(Collectors.toSet());
+    }
 }
