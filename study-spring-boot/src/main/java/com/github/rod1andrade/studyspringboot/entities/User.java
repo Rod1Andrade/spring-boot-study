afb904cedfdf5256eb6cd445ed1941a486af5fbe
Rodrigo Andrade <rodrigopires.profissional@gmail.com>
2022-01-27 18:23:08 -0300

    UPDATE - Production Heroku deploy config.

--- study-spring-boot/src/main/java/com/github/rod1andrade/studyspringboot/entities/User.java
+++ study-spring-boot/src/main/java/com/github/rod1andrade/studyspringboot/entities/User.java
@@ -1,6 +1,7 @@
 package com.github.rod1andrade.studyspringboot.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
 
 import java.io.Serial;
 import java.io.Serializable;
@@ -16,6 +17,10 @@ import javax.persistence.*;
  * @author Rodrigo Andrade
  */
 @Entity(name = "tb_user")
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@ToString
 public class User implements Serializable {
 
 	@Serial
@@ -23,96 +28,30 @@ public class User implements Serializable {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	@Setter
+	@EqualsAndHashCode.Include
 	private Long id;
-	
+
+	@Getter
+	@Setter
 	private String name;
+
+	@Getter
+	@Setter
 	private String email;
+
+	@Getter
+	@Setter
 	private String phone;
 
-	@JsonIgnore
+	@Getter
+	@Setter
 	private String password;
 
 	@OneToMany(mappedBy = "client")
 	@JsonIgnore
+	@Getter
 	private final List<Order> orders = new ArrayList<>();
-
-	public User() {
-	}
-
-	public User(Long id, String name, String email, String phone, String password) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.email = email;
-		this.phone = phone;
-		this.password = password;
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getPhone() {
-		return phone;
-	}
-
-	public void setPhone(String phone) {
-		this.phone = phone;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public List<Order> getOrders() {
-		return orders;
-	}
-
-	@Override
-	public int hashCode() {
-		return Objects.hash(id);
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		User other = (User) obj;
-		return Objects.equals(other.id, id);
-	}
-
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", password=" + password
-				+ "]";
-	}
 
 }
